eb67d0bb634d6ad5bb7c17f0e749732b206a1cd0
Naveen Gowda <117651142+naveengowda7@users.noreply.github.com>
2023-11-22 17:55:14 +0000

    Datatype file pushed

---
+++ 01_basics/01_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+let name = "Naveen"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// Primitive Data Types
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined); // type => undefined
+console.log(typeof null); // type => Object
---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 23042
+let accountEmail = "naveengowda23277@gmail.com"
+var accountPassword = "1234567"
+var accountCity = "Bengaluru"
+let accountState;
+// accountId = 2 // not allowed
+// In JavaScript var is not used..
+
+accountEmail = "naveen@google.com"
+accountPassword = "21212112"
+accountCity = "Mangalore"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
